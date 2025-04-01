8509c0e2b846ce029cc608006ae3db1bd47febd0
AlanLM18 <alanlm.ti23@utsjr.edu.mx>
2025-04-01 12:27:59 -0600

    index.js

--- index.js
+++ index.js
@@ -3,25 +3,29 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import usuariosRutas from "./routes/usuarioRutas.js";
 import { conectarBD } from "./db/db.js";
-// Importar el cliente MQTT
-import {client as mqttClient} from "./mqtt/mqtt.js";
+import { client as mqttClient } from "./mqtt/mqtt.js";
+import 'dotenv/config'; // Cargar variables de entorno
 
 const app = express();
+
+// Conectar a la base de datos
 conectarBD();
 
-// Configuración de middleware Express
+// Configuración de middleware
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Configurar CORS para permitir solicitudes con credenciales
 app.use(cors({
-    origin: true, 
+    origin: true,
     credentials: true
 }));
 
 // Hacer disponible el cliente MQTT en las rutas
 app.use((req, res, next) => {
-  req.mqttClient = mqttClient;
-  next();
+    req.mqttClient = mqttClient;
+    next();
 });
 
 // Rutas
@@ -30,11 +34,12 @@ app.use("/api", usuariosRutas);
 // Iniciar el servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor express en http://localhost:${PORT}`);
+    console.log(`Servidor Express en http://localhost:${PORT}`);
 });
 
-// Manejar el cierre apropiado
-process.on('SIGINT', () => {
-  mqttClient.end();
-  process.exit();
+// Manejar el cierre apropiado del cliente MQTT
+process.on("SIGINT", () => {
+    console.log("Cerrando conexión MQTT...");
+    mqttClient.end();
+    process.exit();
 });
